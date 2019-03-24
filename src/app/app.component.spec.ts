ba463a85f670745b20614cfb4c4b7ba37f87a757
jamesandere <jamesandere56@gmail.com>
2019-03-24 11:16:16 +0300

    Create component files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'quo'`, () => {
+  it(`should have as title 'quotes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('quo');
+    expect(app.title).toEqual('quotes');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to quo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to quotes!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FormsComponent } from './forms/forms.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { DetailsComponent } from './details/details.component';
+import { CountPipePipe } from './count-pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent,
+    QuotesComponent,
+    DetailsComponent,
+    CountPipePipe,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
